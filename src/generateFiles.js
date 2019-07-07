2bcb3d426502dc7d7412cff24fb17337273e1ed8
Andrew Serra <andy@serra.us>
2019-07-07 22:42:55 +0300

    code climate fixes

--- src/generateFiles.js
+++ src/generateFiles.js
@@ -15,27 +15,27 @@ const { setupPath } = require('./generateArray');
 // @TODO I don't like that we have all of these path manipulations inside of this method
 // folderData = './src/data/'
 function generateFiles(pathToSrc) {
+  let path;
+  let data;
+  setupPath(pathToSrc);
 
-    var path, data;
-    setupPath(pathToSrc);
+  config.map((settings) => {
+    const fileName = settings.name;
+    const uppercaseFileName = fileName.charAt(0).toUpperCase();
 
-    config.map((settings) => {
-      let fileName = settings.name;
-      let uppercaseFileName = fileName.charAt(0).toUpperCase();
+    const folder = uppercaseFileName.concat(fileName.slice(1));
+    //   var path = './output/' + fileName + '.json';
+    const folderPath = `${pathToSrc}/data/${folder}`;
 
-      let folder = uppercaseFileName.concat(fileName.slice(1));
-      //   var path = './output/' + fileName + '.json';
-      let folderPath = `${pathToSrc}/data/${folder}`;
+    if (isDirectory(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
 
-      if (isDirectory(folderPath)) {
-        fs.mkdirSync(folderPath);
-      }
+    path = `${folderPath}/${fileName}.json`;
+    data = settings.data;
 
-      path = `${folderPath}/${fileName}.json`;
-      data = settings.data;
-
-      writeFile(path, data);
-    });
+    writeFile(path, data);
+  });
 }
 
 module.exports = {

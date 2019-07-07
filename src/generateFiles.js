6220cb4830c1b0cc7866a4a9c68206192ef6892e
Andrew Serra <andy@serra.us>
2019-07-07 21:08:19 +0300

    changed warnings of codeclimate for issue 99

--- src/generateFiles.js
+++ src/generateFiles.js
@@ -8,38 +8,34 @@ const { writeFile, readData } = require('./writeFile');
 //   getMeasurementSystem,
 //   getMeasurementUnits
 // } = require('./generateArray')
-
-
-
-const {
-  config, setupPath
-} = require('./configGenerator')
+const { isDirectory } = require('./utils');
+const { config } = require('./configGenerator');
+const { setupPath } = require('./generateArray');
 
 // @TODO I don't like that we have all of these path manipulations inside of this method
 // folderData = './src/data/'
-function generateFiles (pathToSrc) {
-    // console.log(config[0]["data"]())
-    setupPath(pathToSrc)
+function generateFiles(pathToSrc) {
 
-    config.map(settings => {
-      var fileName = settings['name']
-      // @TODO I don't like this long line
-      var folder = fileName.charAt(0).toUpperCase() + fileName.slice(1)
+    var path, data;
+    setupPath(pathToSrc);
+
+    config.map((settings) => {
+      let fileName = settings.name;
+      let uppercaseFileName = fileName.charAt(0).toUpperCase();
+
+      let folder = uppercaseFileName.concat(fileName.slice(1));
       //   var path = './output/' + fileName + '.json';
-      var folderPath = pathToSrc +'/data/' + folder
+      let folderPath = `${pathToSrc}/data/${folder}`;
 
-      // @TODO use isDirectory?
-      if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath)
+      if (isDirectory(folderPath)) {
+        fs.mkdirSync(folderPath);
       }
 
-      var path = folderPath + '/' + fileName + '.json'
-      var data = settings['data']()
+      path = `${folderPath}/${fileName}.json`;
+      data = settings.data;
 
-      writeFile(path, data)
-    })
-
-
+      writeFile(path, data);
+    });
 }
 
 module.exports = {

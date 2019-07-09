453b26ec6551c9efa50e3bace11753591259e3fe
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-09 23:00:09 +0000

    clean up - generateFiles works at sd repository

--- src/generateFiles.js
+++ src/generateFiles.js
@@ -5,20 +5,20 @@ import config from './configGenerator';
 import { setupPath } from './generateArray';
 import { setupPathMeasurements } from './measurements';
 
-console.log('generate file start');
+// console.log('generate file start');
 
 // @TODO I don't like that we have all of these path manipulations inside of this method
 // folderData = './src/data/'
 const generateFiles = (pathToSrc) => {
   let path;
-  console.log('1');
+//   console.log('1');
   setupPath(pathToSrc);
   setupPathMeasurements(pathToSrc);
-  console.log('2');
-  
+//   console.log('2');
+
   config.map((settings) => {
     const { fileName, data } = settings;
-    console.log('3');
+    // console.log('3');
       const uppercaseFileName = fileName.charAt(0).toUpperCase();
 
       const folder = uppercaseFileName.concat(fileName.slice(1));
@@ -30,7 +30,7 @@ const generateFiles = (pathToSrc) => {
       }
 
       path = `${folderPath}/${fileName}.json`;
-      console.log('4');
+    //   console.log('4');
       write(path, data());
   });
 };

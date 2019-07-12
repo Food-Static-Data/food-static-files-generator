80a58fe6efbf96a1df29bd341780e43af210c662
Andrew Serra <andy@serra.us>
2019-07-13 00:56:44 +0300

    corrected mistake

--- src/tests/fileSystem.test.js
+++ src/tests/fileSystem.test.js
@@ -1,7 +1,22 @@
-import { write } from '../fileSystem';
+import { write, save, makeFolder } from '../fileSystem';
 
-describe('testing fileSystem', () =>{
-    test('testing function write()', () => {
-        write('./output/test.json',[{'name':'Test'}])
-    })
+describe('testing fileSystem()', () => {
+  test('testing function write()', () => {
+    write('./output/test.json', [{
+      'name': 'Test'
+    }], status => {
+      expect(status).toBe(true);
+    });
+  });
+
+  test('testing function save()', () => {
+    save('./output/', 'test.json', "[{'name':'Test'}]", false, status => {
+      expect(status).toBe(true);
+    });
+  });
+
+  test('testing makeFolder()', () => {
+    const result = makeFolder('./output/', 'test.json');
+    expect(result).toBe('./output/test_elements');
+  })
 })

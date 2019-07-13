5663d8ccf137348a341189b7612cdd3cafb43fbd
Amogh J <amoghjahagirdargsf@gmail.com>
2019-07-13 10:50:38 +0400

    moved readData to fileSystem

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -1,7 +1,7 @@
 // here i want to keep methods like write, save
 // we can also rename WriteFile to write
 // saveFile to save - because we didn't save or wrtie anything else here
-import { writeFile, mkdirSync } from 'fs';
+import { writeFile, mkdirSync, readFileSync } from 'fs';
 import { stripSymbols, getFileName } from './writeFile';
 import { isDirectory } from './utils';
 

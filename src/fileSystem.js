28012beb5fbf014059e2685f2a33b77328949c2d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-14 21:03:01 +0300

    Update fileSystem.js

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -1,6 +1,3 @@
-// here i want to keep methods like write, save
-// we can also rename WriteFile to write
-// saveFile to save - because we didn't save or wrtie anything else here
 import { writeFile, mkdirSync, readFileSync } from 'fs';
 import { stripSymbols, getFileName } from './writeFile';
 import { isDirectory } from './utils';

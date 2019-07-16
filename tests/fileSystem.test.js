bbfea8bc44783cb1bcbe62809366b525673695a7
svr8 <shikhar.vaish90@gmail.com>
2019-07-16 13:57:13 +0530

    update fileSystem tests

--- tests/fileSystem.test.js
+++ tests/fileSystem.test.js
@@ -1,17 +1,19 @@
 import { write, save, makeFolder } from '../src/fileSystem';
 
 describe('testing fileSystem()', () => {
-  test('testing function write()', () => {
+  test('testing function write()', done => {
     write('./output/test.json', [{
       'name': 'Test'
     }], status => {
       expect(status).toBe(true);
+      done();
     });
   });
 
-  test('testing function save()', () => {
+  test('testing function save()', done => {
     save('./output/', 'test.json', "[{'name':'Test'}]", false, status => {
       expect(status).toBe(true);
+      done();
     });
   });
 

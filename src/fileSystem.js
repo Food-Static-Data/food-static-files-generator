3884f67fd449c11a99a982a50ae6e67e1d542d0f
Saphal Patro <saphal1998@gmail.com>
2019-07-19 18:29:21 +0530

    trailing spaces

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -37,7 +37,7 @@ const read = (absolutePath) => {
   const data = readFileSync(absolutePath);
   if (data === '') {
     console.log(`${absolutePath} returned empty`);
-  } 
+  }
   console.log(data);
   const fileData = JSON.parse(data);
   return fileData;

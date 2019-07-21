58f3a8ba4d81eeeece1a86ca1f58828bc00301c0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-21 23:27:51 +0000

    babel config extension

--- babel.config.js
+++ babel.config.js
@@ -1,47 +1,52 @@
-'use strict'
+"use strict";
 
 // const path = require('path')
 
 const presets = [
-  ['@babel/preset-env', {
-    'modules': false
-  }]
-]
+  [
+    "@babel/preset-env",
+    {
+      modules: false
+    }
+  ]
+];
 
 // Add this to working tests
 const env = {
   test: {
-    presets: [['@babel/preset-env', {
-      "targets": {
-        "node": "current"
-      }
-    }
-  ]]
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            node: "current"
+          }
+        }
+      ]
+    ]
   }
-}
+};
 
 const plugins = [
   [
-    require.resolve('babel-plugin-module-resolver'),
+    require.resolve("babel-plugin-module-resolver"),
     {
-      root: ['./src/'],
+      root: ["./src/"],
       alias: {
-        '~': './src/data'
+        "~": "./src/data"
       }
     }
   ],
+  ["@babel/plugin-syntax-dynamic-import"],
+  ["@babel/plugin-proposal-object-rest-spread"],
+  ["@babel/plugin-transform-async-to-generator"],
   [
-    '@babel/plugin-syntax-dynamic-import'
-  ],
-  [
-    '@babel/plugin-proposal-object-rest-spread'
-  ],
-  [
-    '@babel/plugin-transform-async-to-generator'
-  ],
-  [
-    '@babel/plugin-transform-runtime'
+    "@babel/plugin-transform-runtime",
+    {
+      helpers: true,
+      regenerator: true
+    }
   ]
-]
+];
 
-module.exports = { presets, plugins, env }
+module.exports = { presets, plugins, env };

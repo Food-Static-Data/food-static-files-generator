b073aa3ee3a787876b6ca32a93b69d15dfbb6988
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-08-17 18:37:03 +0300

    Update fileSystem.test.js

--- tests/fileSystem.test.js
+++ tests/fileSystem.test.js
@@ -1,5 +1,14 @@
 import { write, save, makeFolder } from '../src/fileSystem';
 
+
+//@todo I think we should separate our tests into different describe blocks.
+// so it will look more clear. Right now our block related to fileSystem file, not to a method as it stated.
+// 1. have 3 describe blocks for our methods.
+// 2. have different explanations at describe blocks
+
+// also one of our methods is not finished/upgraded in order to use async way.
+// tests for these methods are very important, because we're using these methods frequently and highly rely on them
+
 describe('testing fileSystem()', () => {
   // test write()
   test('testing function write()', async () => {

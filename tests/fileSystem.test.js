01375cc836ab33f18238d75fb05070a93492a9a4
oksanafrankiv <oksanafrankiv@gmail.com>
2019-09-03 20:36:22 -0700

    Updated tests

--- tests/fileSystem.test.js
+++ tests/fileSystem.test.js
@@ -1,32 +1,51 @@
-import { write, save, makeFolder } from '../src/fileSystem';
+import fs from "fs";
+import { write, save, makeFolder } from "../src/fileSystem";
 
+describe("testing fileSystem", () => {
+  const testFolder = "./output/";
+  const testFile = "test.json";
+  const testFullPath = testFolder + testFile;
+  const testFileContent = [{ name: "Test" }];
 
-// @todo I think we should separate our tests into different describe blocks.
-// so it will look more clear. Right now our block related to fileSystem file, not to a method as it stated.
-// 1. have 3 describe blocks for our methods.
-// 2. have different explanations at describe blocks
+  describe("testing function write()", () => {
+    // test write()
+    test("testing successul case", async () => {
+      const result = await write(testFullPath, testFileContent);
+      expect(result).toBe(true);
+      expect(fs.existsSync(testFullPath)).toBe(true);
+      fs.unlinkSync(testFullPath);
+    });
 
-// also one of our methods is not finished/upgraded in order to use async way.
-// tests for these methods are very important, because we're using these methods frequently and highly rely on them
+    test("testing invalid file name", async () => {
+      await expect(write(null, testFileContent)).rejects.toBeTruthy(); // throw an exception
+    });
 
-describe('testing fileSystem()', () => {
-  // test write()
-  test('testing function write()', async () => {
-    const result =  await write('./output/test.json', [{
-      'name': 'Test'
-    }]);
-    expect(result).toBe(true);
+    test("testing null content", async () => {
+      const result = await write(testFullPath, null);
+      expect(result).toBe(true);
+      expect(fs.existsSync(testFullPath)).toBe(true);
+      fs.unlinkSync(testFullPath);
+    });
   });
 
-  // test save()
-  test('testing function save()', async () => {
-    const result = await save('./output/', 'test.json', "[{'name':'Test'}]", false);
-    expect(result).toBe(true);
+  describe("testing function save()", () => {
+    // test save()
+    test("testing function save()", async () => {
+      const result = await save(
+        testFolder,
+        testFile,
+        testFileContent.toString(),
+        false
+      );
+      expect(result).toBe(true);
+    });
   });
 
-  // test makeFolder()
-  test('testing makeFolder()', () => {
-    const result = makeFolder('./output/', 'test.json');
-    expect(result).toBe('./output/test_elements');
-  })
-})
+  describe("testing function makeFolder()", () => {
+    // test makeFolder()
+    test("testing makeFolder()", async () => {
+      const result = await makeFolder(testFolder, testFile);
+      expect(result).toBe(`${testFolder}test_elements`);
+    });
+  });
+});

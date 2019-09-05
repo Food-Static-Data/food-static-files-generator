5ed70e38e4084f555c92adeeb78e2ff4bfe158fb
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-09-05 08:07:09 +0300

    clean up

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,10 +3,9 @@
 //   "after": "lint-staged -c ./lint-staged.config.js",
 //   "then": "yarn precommit"
 // }
+
 module.exports = {
   hooks: {
-    "pre-commit":
-      // "lint-staged -c lint-staged.config.js"
-      "lint-staged -c lint-staged.config.js"
+    "pre-commit": "lint-staged -c lint-staged.config.js"
   }
 };

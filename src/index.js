f6ff1ed8f8e2a2af160ee5426b90b66970274458
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-09-15 23:09:31 +0300

    re-organisation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import generateFiles from './generateFiles';
+// import generateFiles from './generateFiles';
 // @TODO not sure if we need to export it
 import { getFileInfo, getListContent } from './utils';
 import { combine, split } from './objects';
 import { read, write, isFolderExists } from './fileSystem';
 
 export {
-  generateFiles,
+  // generateFiles,
   combine,
   split,
   write,

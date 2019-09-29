913ff275b26c5093574816e6781d31e8de25f603
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-09-29 22:46:27 +0300

    Update index.js

--- projects3.0/WeeklyMenu/server/index.js
+++ projects3.0/WeeklyMenu/server/index.js
@@ -2,7 +2,14 @@ const { write } = require("@groceristar/static-data-generator");
 
 const { getMenuGenerator } = require("./methods");
 
-// for running quick tests
+// 1. for running quick tests
 //  write("./output/test.json", [{ name: "Test" }]);
+
+// 2. just testing write method with more real data
 const generatedFilesPath = "./output/test.json";
 write(generatedFilesPath, getMenuGenerator);
+
+
+// 3. using generateFile method here instead of previous versions just for testing how it works
+// const generateFile = require('./generateFile')
+// generateFile();

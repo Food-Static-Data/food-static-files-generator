78a3e2198f833f76b422019ea6a91133663dff29
Saphal Patro <saphal1998@gmail.com>
2019-10-12 22:16:59 +0530

    making changes in measurement, tests working

--- projects/Measurements/server/methods.js
+++ projects/Measurements/server/methods.js
@@ -1,65 +1,59 @@
-// @groceristar/data-methods-measurements
-//
-// const _ = require("lodash");
-// import {
-//   //   setupPath,
-//   //   readAllFiles,
-//   generateArrWithId
-// } from "@groceristar/static-data-generator";
-//
-// const {
-//   measurementSystem,
-//   measurementUnits
-// } = require("@groceristar/sd-wrapper");
-//
+const _ = require("lodash");
+const { generateArrWithId } = require("@groceristar/static-data-generator");
+
+const {
+  measurementSystem,
+  measurementUnits
+} = require("@groceristar/sd-wrapper");
+
 // // const setupPathMeasurements = pathToSrc => {
 // //     files = require(pathToSrc + '/files');
 // // };
-//
-// const getMeasurementSystem = () => {
-//   // const files = setupPath("../../sd/src");
-//   const result = [];
-//   const measurementSystemId = generateArrWithId(measurementSystem, "id");
-//
-//   _.map(measurementSystemId, system => {
-//     result.push({
-//       id: system.id,
-//       alias: system.alias,
-//       title: _.capitalize(system.alias)
-//     });
-//   });
-//
-//   return result;
-// };
-//
-// const getMeasurementUnits = () => {
-//   // const files = setupPath("../../sd/src");
-//   const dirMeasurementUnits = parse(measurementUnits).dir;
-//   const result = [];
-//
-//   let measurementUnitsList = readAllFiles(dirMeasurementUnits)[1];
-//
-//   measurementUnitsList = generateArrWithId(measurementUnitsList, "id");
-//   measurementUnitsList = generateArrWithId(measurementUnitsList, "system_id");
-//
-//   _.map(measurementUnitsList, unit => {
-//     result.push({
-//       id: unit.id,
-//       system_id: unit.system_id,
-//       type: unit.type,
-//       name: unit.name,
-//       singular: unit.singular,
-//       plural: unit.plural,
-//       short: unit.short,
-//       pattern: unit.pattern,
-//       error: "null"
-//     });
-//   });
-//   return result;
-// };
-//
-// module.exports = {
-//   // setupPathMeasurements,
-//   getMeasurementSystem,
-//   getMeasurementUnits
-// };
+
+const getMeasurementSystem = () => {
+  // const files = setupPath("../../sd/src");
+  const result = [];
+  const measurementSystemId = generateArrWithId(measurementSystem, "id");
+
+  _.map(measurementSystemId, system => {
+    result.push({
+      id: system.id,
+      alias: system.alias,
+      title: _.capitalize(system.alias)
+    });
+  });
+
+  return result;
+};
+
+const getMeasurementUnits = () => {
+  // const files = setupPath("../../sd/src");
+  const dirMeasurementUnits = parse(measurementUnits).dir;
+  const result = [];
+
+  let measurementUnitsList = readAllFiles(dirMeasurementUnits)[1];
+
+  measurementUnitsList = generateArrWithId(measurementUnitsList, "id");
+  measurementUnitsList = generateArrWithId(measurementUnitsList, "system_id");
+
+  _.map(measurementUnitsList, unit => {
+    result.push({
+      id: unit.id,
+      system_id: unit.system_id,
+      type: unit.type,
+      name: unit.name,
+      singular: unit.singular,
+      plural: unit.plural,
+      short: unit.short,
+      pattern: unit.pattern,
+      error: "null"
+    });
+  });
+  return result;
+};
+
+module.exports = {
+  // setupPathMeasurements,
+  getMeasurementSystem,
+  getMeasurementUnits
+};
